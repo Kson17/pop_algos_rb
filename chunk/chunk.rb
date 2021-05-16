de52944e1d65b337898d3c9366808ee12c4aeca0
Alexey Katasonov <alexey.kson@gmail.com>
2021-05-16 22:36:07 +0300

    second option of solving chunking

--- chunk/chunk.rb
+++ chunk/chunk.rb
@@ -10,7 +10,7 @@ chunk([1,2,3,4,5,6,7], 3) => [[1,2,3], [4,5,6], [7]]
 
 =end
 
-def chunk(array, size)
+def chunk_1(array, size)
   chunked = []
 
   array.each do |e|
@@ -19,6 +19,18 @@ def chunk(array, size)
     else
       chunked.last << e
     end
+  end
+
+  chunked
+end
+
+def chunk_2(array, size)
+  chunked = []
+  index = 0
+
+  while index < array.size do
+    chunked << array[index...index+size]
+    index += size
   end
 
   chunked
